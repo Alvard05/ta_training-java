c813fb0d56dcd0b8584e4756b8ef7b128f908657
alvard babakhanyan <alvard.babakhanyan@gmail.com>
2022-02-10 20:45:38 +0400

    second commit

--- src/Main_task/HelloUser.java
+++ src/Main_task/HelloUser.java
@@ -2,12 +2,11 @@ package Main_task;
 
 public class HelloUser {
     public static void main(String[] args) {
-       int argslength = 2;
+        int i = 2;
+       int argslength = i;
       int [] array = new int [args.length];
-     if (argslength >= 2) {
-          //System.out.println("argument 0 = " + args[0]);
-       // System.out.println( " args[0]  " + args[1] );
-      //System.out.println(" Hello " + " " + " argument(1) ");
+     for (argslength = 2; i >= 2; i++ ); {
+
      }
            System.out.println(" Hello " + args[1] );
 
