99bea3a0c5e642b5b0aa73a46f62c03a679e0d67
alvard babakhanyan <alvard.babakhanyan@gmail.com>
2022-02-10 20:22:24 +0400

    first commit

---
+++ src/Main_task/HelloUser.java
@@ -0,0 +1,15 @@
+package Main_task;
+
+public class HelloUser {
+    public static void main(String[] args) {
+       int argslength = 2;
+      int [] array = new int [args.length];
+     if (argslength >= 2) {
+          //System.out.println("argument 0 = " + args[0]);
+       // System.out.println( " args[0]  " + args[1] );
+      //System.out.println(" Hello " + " " + " argument(1) ");
+     }
+           System.out.println(" Hello " + args[1] );
+
+       }
+}
--- src/com/epam/training/student_alvard_babakhanyan/Main.java
+++
@@ -1,7 +0,0 @@
-package com.epam.training.student_alvard_babakhanyan;
-
-public class Main {
-    public static void main(String[] args) {
-
-    }
-}
