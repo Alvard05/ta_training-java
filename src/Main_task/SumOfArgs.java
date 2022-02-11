21d7332f9d4ca52c58e94653cf4a124ab9d58f4d
alvard babakhanyan <alvard.babakhanyan@gmail.com>
2022-02-11 18:01:37 +0400

    the sum of numbers

---
+++ src/Main_task/SumOfArgs.java
@@ -0,0 +1,13 @@
+package Main_task;
+
+public class SumOfArgs {
+    public static void main(String[] args) {
+
+
+        int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+        int sum = a + b;
+        System.out.println("the sum of a and b is: " + sum);
+
+    }
+}
