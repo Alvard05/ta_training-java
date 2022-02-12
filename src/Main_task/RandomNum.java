3ff994d27a4cfa8fef3b1284c26ac71655a5b823
alvard babakhanyan <alvard.babakhanyan@gmail.com>
2022-02-13 00:01:29 +0400

    last commit

--- src/Main_task/RandomNum.java
+++ src/Main_task/RandomNum.java
@@ -1,14 +1,12 @@
 package Main_task;
-
 public class RandomNum {
     public static void main(String[] args) {
         int min  = 0;
         int max = 50;
-        for (int i = 0; i < 5 ; i++) {
+        int count = Integer.parseInt(args[0]);
+        for (int i = 0; i < count ; i++) {
             int random_number = min + (int) (Math.random() * max);
             System.out.println(" " + random_number);
         }
-
-
     }
 }
