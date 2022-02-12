238e818feea4f06901002f67942486b3e0cbd2bc
alvard babakhanyan <alvard.babakhanyan@gmail.com>
2022-02-12 23:59:50 +0400

    month number commit

---
+++ src/Main_task/Months.java
@@ -0,0 +1,17 @@
+package Main_task;
+public class Months {
+    public static void main(String[] args) {
+
+        String arr[] = {"January", "February", "March", "April", "May", "June", "Jule",
+                "August", "September", "October", "November", "December"};
+        int index = Integer.parseInt(args[0]);
+        System.out.println(arr[index - 1]);
+
+
+    }
+
+}
+
+
+
+
