6ba5cc38c6ea6186d1ada733a2441f2b7e8d40dd
alvard babakhanyan <alvard.babakhanyan@gmail.com>
2022-02-23 10:47:39 +0400

    commit

--- src/Main_task/HelloUser.java
+++ src/Main_task/HelloUser.java
@@ -1,13 +1,9 @@
 package Main_task;
-
 public class HelloUser {
     public static void main(String[] args) {
-       int argslength;
 
-     for (argslength = 1; argslength >= 1; argslength++ ); {
-
-     }
-     System.out.println(" Hello " + args[0] );
-
-       }
+        for (int argslength = 1; argslength >= 1; argslength++){
+        }
+        System.out.println(" Hello " + args[0] );
+    }
 }
