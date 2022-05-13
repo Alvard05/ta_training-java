df4ee871dfdd5807505d7c68d733e61ff26d4586
alvard babakhanyan <alvard.babakhanyan@gmail.com>
2022-05-13 17:48:03 +0400

    commit

--- src/com/epam/training/student_alvard_babakhanyan/collections/main_task/FindingFlowers.java
+++ src/com/epam/training/student_alvard_babakhanyan/collections/main_task/FindingFlowers.java
@@ -1,2 +1,8 @@
-package com.epam.training.student_alvard_babakhanyan.collections.main_task;public interface FindingFlowers {
+package com.epam.training.student_alvard_babakhanyan.collections.main_task;
+
+import java.util.List;
+
+public interface FindingFlowers {
+    List<Flower> findingFlowerByLengthOfStem(int minStemLength, int maxStemLength);
+    List<Flower> collectBouquet(String name, int count);
 }
